30770441fef8f856aa2a407c19c78ea1e5439ee8
Marycarmen Mora <137240642+muredesigner@users.noreply.github.com>
2023-07-28 16:47:30 -0400

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -5,3 +5,28 @@ let contacts = [
   {name: "Enedina Mora"},
   {name: "Andres Montenegro"},
 ]
+
+// Función para añadir un nuevo contacto a una lista
+
+function addContact(name) {
+  contacts.push({ name });
+}
+
+// Función para eliminar un contacto existente de la lista
+
+function deleteContact(name) {
+  contacts = contacts.filter(contact => contact.name !== name);
+}
+
+// Función para imprimir los contactos presentes en la lista
+
+function printContacts() {
+  console.log("Contacts:");
+  contacts.forEach(contact => console.log(contact.name));
+}
+
+// Ejemplo de utilización
+
+addContact("Rafael Montes");                   // Añade un contacto
+deleteContact("Enedina Mora");                 // Elimina un contacto
+printContacts();
