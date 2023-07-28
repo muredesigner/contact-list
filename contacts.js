1a459c6680162344d43c2e239686903c808384db
Marycarmen Mora <137240642+muredesigner@users.noreply.github.com>
2023-07-28 16:54:04 -0400

    bonus para actualizar un contacto existente

--- contacts.js
+++ contacts.js
@@ -1,32 +1,68 @@
-// Lista de contactos con datos predefinidos
+/* Ajusta las funciones de crear y eliminar un contacto para
+que puedan almacenar la siguiente información como objetos:
+id, nombres, apellidos, teléfono, ubicaciones, ciudad, dirección */
 
 let contacts = [
-  {name: "Simon Bastidas"},
-  {name: "Enedina Mora"},
-  {name: "Andres Montenegro"},
-]
+  {
+      id: 1,
+      firstName: 'Simon',
+      lastName: 'Bastidas',
+      phone: '1234567890',
+      location: {
+          city: 'Tachira',
+          address: 'Av. Carabobo'
+      }
+  },
+  {
+      id: 2,
+      firstName: 'Enedina',
+      lastName: 'Mora',
+      phone: '098765421',
+      location: {
+          city: 'Portuguesa',
+          address: 'Av. Hilandera'
+      }
+  }
+];
 
-// Función para añadir un nuevo contacto a una lista
+/* función para añadir un nuevo contacto a la lista pasando el id, 
+firstName, lastName, teléfono, ciudad y dirección como argumentos. */
 
-function addContact(name) {
-  contacts.push({ name });
+function addContact(id, firstName, lastName, phone, city, address) {
+  contacts.push({
+      id: id,
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      location: {
+          city: city,
+          address: address
+      }
+  });
 }
 
-// Función para eliminar un contacto existente de la lista
+/* función para eliminar un contacto existente de la lista se
+puede utilizar pasando el id del contacto que vayamos a eliminar. */
 
-function deleteContact(name) {
-  contacts = contacts.filter(contact => contact.name !== name);
+function deleteContact(id) {
+  contacts = contacts.filter(contact => contact.id !== id);
 }
 
-// Función para imprimir los contactos presentes en la lista
+/* Funcion para imprimir en la consola el nombre y apellidos
+de los contactos que esten presentes en la lista. */
 
 function printContacts() {
-  console.log("Contacts:");
-  contacts.forEach(contact => console.log(contact.name));
+  console.log('Contacts:');
+  contacts.forEach(contact => console.log(`${contact.firstName} ${contact.lastName}`));
 }
 
-// Ejemplo de utilización
+/* Para actualizar un contacto existente en la lista pasando el nombre y apellidos antiguos
+como una única cadena y el nombre y apellidos nuevos como una única cadena. La función 
+encontrará el contacto con el nombre antiguo y lo actualizará con el nombre nuevo. */
 
-addContact("Rafael Montes");                   // Añade un contacto
-deleteContact("Enedina Mora");                 // Elimina un contacto
-printContacts();
+function updateContact(oldName, newName) {
+    let index = contacts.findIndex(contact => contact.name === oldName);
+    if (index !== -1) {
+        contacts[index].name = newName;
+    }
+}
